fa6034378d269a95d4b725cfef0f7cc001c7cc5d
Derek Peacock <derek.peacock@yahoo.com>
2021-12-06 15:07:42 +0000

    added dbinitialiser

---
+++ Code/Razor Pages/blockbusters/Data/DbInitiaiser.cs
@@ -0,0 +1,58 @@
+﻿using blockbusters.Models;
+using System;
+using System.Linq;
+
+namespace blockbusters.Data
+{
+    public static class DbInitiaiser
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            AddPayments(context);
+            AddPeople(context);
+        }
+
+        private static void AddPeople(ApplicationDbContext context)
+        {
+            // Look for any students.
+            if (context.People.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var people = new Person[]
+            {
+               new Person()
+               {
+                   FirstName="",
+               }
+            };
+
+            context.People.AddRange(people);
+            // context.SaveChanges();
+        }
+
+        private static void AddPayments(ApplicationDbContext context)
+        {
+            // Look for any students.
+            if (context.Payments.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var payments = new Payment[]
+            {
+                new Payment()
+                {
+                    CardholderName="Bob Smith", 
+                    CardNumber="1234567812345678",
+                    ExpirationDate = "1024",
+                    CCV="234"
+                }
+            };
+
+            context.Payments.AddRange(payments);
+            // context.SaveChanges();
+        }
+    }
+}
