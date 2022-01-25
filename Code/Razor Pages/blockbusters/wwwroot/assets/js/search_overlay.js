6d31440a01de71d914869bb51160a468d5aa93a7
hsadiq32 <20135112s@gmail.com>
2022-01-25 23:59:03 +0000

    stitching design

---
+++ Code/Razor Pages/blockbusters/wwwroot/assets/js/search_overlay.js
@@ -0,0 +1,21 @@
+function searchOn() {
+  document.getElementById("SearchOverlay").style.opacity = "100%";
+  document.getElementById("SearchOverlay").style.visibility = "visible";
+
+}
+
+function searchOff() {
+  document.getElementById("SearchOverlay").style.visibility = "hidden";
+  document.getElementById("SearchOverlay").style.opacity = "0%";
+}
+
+function userOn() {
+  document.getElementById("UserOverlay").style.opacity = "100%";
+  document.getElementById("UserOverlay").style.visibility = "visible";
+
+}
+
+function userOff() {
+  document.getElementById("UserOverlay").style.visibility = "hidden";
+  document.getElementById("UserOverlay").style.opacity = "0%";
+}
